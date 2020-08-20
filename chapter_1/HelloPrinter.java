924fdf1ef4ee1f18f077b8ff867f3e6b8eadb66d
ianbooth <off.ibooth@gmail.com>
2020-08-20 11:09:30 -0700

    Chapter 1 Material and Review Sections

---
+++ chapter_1/HelloPrinter.java
@@ -0,0 +1,15 @@
+public class HelloPrinter
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, World!"); // Prints "Hello, World!"
+
+		System.out.println(3 + 4); // Also performs math equations
+
+		System.out.println("Hello");
+		System.out.println("World!"); // Indent btw words
+
+		System.out.println("Goodbye");
+	}
+}
+
