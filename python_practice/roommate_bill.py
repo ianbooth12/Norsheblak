bae9b0f5dacf28b2b0261768cb2449aabe62468e
ianbooth <off.ibooth@gmail.com>
2020-08-20 11:09:04 -0700

    Using Python to conceptually understand

---
+++ python_practice/roommate_bill.py
@@ -0,0 +1,15 @@
+roommates = ["diana", "juliet", 'mary', 'romeo', 'tom']
+cable_viewers = ['diana', 'mary', 'tom']
+rent = 1800
+electricity = 82.54
+cable = 40
+gas = 281.40
+cable_share = cable / len(cable_viewers)
+electricity_share = electricity / len(roommates)
+rent_share = rent / len(roommates)
+gas_share = gas / len(roommates)
+for roomie in roommates:
+	roomie = {"bill": 0}
+if roomie in roommates:
+print(diana)
+
