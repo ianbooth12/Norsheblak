10d12f87a52cd3fde46b321fa6f5e72d3802465a
ianbooth <off.ibooth@gmail.com>
2020-09-03 18:53:40 -0700

    File to read and return name in Last, First format

--- chapter_2/Names.java
+++ chapter_2/Names.java
@@ -4,9 +4,11 @@ public class Names
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
+		System.out.print("Jamal said\"Go to my website tit.com\\nore");
 		System.out.print("Enter your first and last name: ");
 		String firstName = in.next();
 		String lastName = in.next();
-		System.out.print(lastName + ", " + firstName);
+		System.out.println(lastName + ", " + firstName);
+		
 	}
 }
