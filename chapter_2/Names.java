8b3ca0f2a46b9eda1e7d209bf5ab359c07d139db
ianbooth <off.ibooth@gmail.com>
2020-09-07 16:02:36 -0700

    Tool for displaying names in (Last, First) format

--- chapter_2/Names.java
+++ chapter_2/Names.java
@@ -4,11 +4,9 @@ public class Names
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
-		System.out.print("Jamal said\"Go to my website tit.com\\nore");
 		System.out.print("Enter your first and last name: ");
 		String firstName = in.next();
 		String lastName = in.next();
-		System.out.println(lastName + ", " + firstName);
-		
+		System.out.println(lastName + ", " + firstName);	
 	}
 }
