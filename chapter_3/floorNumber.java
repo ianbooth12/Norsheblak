bd630d71a22f75a1fc3990858804c06e0ea67c7f
ianbooth <off.ibooth@gmail.com>
2020-09-07 16:03:52 -0700

    commit

---
+++ chapter_3/floorNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class floorNumber
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the floor you live on: ");
+		int floorNumber = in.nextInt();
+		int actualFloor;
+		if (floorNumber > 13)
+		{
+			actualFloor = floorNumber - 1;
+		}
+		else
+		{	
+			actualFloor = floorNumber;
+		}
+		System.out.printf("Press elevator button %d to get to your floor.", actualFloor);
+	}
+}
