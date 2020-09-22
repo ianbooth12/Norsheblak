2ae1d211eacc61f4ec044c62321d4b31ffb6b661
ianbooth <off.ibooth@gmail.com>
2020-09-22 12:59:21 -0700

    Kadin

--- Labs/Lab4.java
+++ Labs/Lab4.java
@@ -10,15 +10,20 @@ public class Lab4
 {
 	public static void main(String[] args)
 	{
-		Scanner in = new Scanner(System.in);
-
+		boolean exit = false;
+		
+		Scanner in = new Scanner(System.in);	
 		System.out.println("Choose an option from the menu: ");
 		System.out.println("1.) Calculate the sum of integers from 1 to a given number.");
 		System.out.println("2.) Calculate the factorial of a number.");
 		System.out.println("3.) Display the leftmost digit of a number.");
 		System.out.println("4.) Quit the program.");
-
 		int option = in.nextInt();
+		
+		if (option == 4);
+		{
+			exit = true;
+		}
 
 		switch(option)
 		{
@@ -26,7 +31,7 @@ public class Lab4
 			System.out.println("Enter a number: ");
 			int sumNumber = in.nextInt();
 			int sum = 0;
-			for (i = 1; i <= sumNumber; i++)
+			for (int i = 1; i <= sumNumber; i++)
 			{
 				sum = sum + i;
 			}
@@ -39,23 +44,23 @@ public class Lab4
 			int product = 1;
 			for (int i = 1; i <= factorialNumber; i++)
 			{
-				product = product * i;
+			product = product * i;
 			}
 			System.out.printf("\nThe factorial of %d is %d.", factorialNumber, product);
 			break;
 
-		case 3:
+			case 3:
 			System.out.println("Enter a number: ");
 			int givenNumber = in.nextInt();
-			for (int i = 1; i >= 10; i++)
+			int leftDigit = givenNumber;
+			while (leftDigit >= 10)
 			{
-				temp / 10;
+				leftDigit = leftDigit / 10;
 			}
-			System.out.printf("\nThe leftmost digit of %d is %d.", givenNumber, temp);
+			System.out.printf("\nThe leftmost digit of %d is %d.", givenNumber, leftDigit);
 			break;
 		case 4:
 			System.out.println("Program exited.");
 		}	
-		
-	}
+	}			
 }
